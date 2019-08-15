8661f252696f76a8d1e131654a544fbb5e785bac
Moise1 <moiseri01@gmail.com>
2019-08-15 20:00:47 +0200

    [Start #167925994] initial commit

--- SERVER/api/v1/routes/userRouter.js
+++ SERVER/api/v1/routes/userRouter.js
@@ -12,7 +12,7 @@ const userRouter = express.Router();
 userRouter.use(json());
 
 userRouter.post("/api/v1/auth/signup", User.userSignUp);
-// userRouter.post("/api/v1/auth/signin", User.userSignIn);
+userRouter.post("/api/v1/auth/signin", User.userSignIn);
 // userRouter.patch("/api/v1/users/:id", tokenExists, userAccess, adminAccess, User.updateUser);
 
 
