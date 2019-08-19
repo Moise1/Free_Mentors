6c153cad443b48fb6e4752f26ff818d02f7be8af
Moise1 <moiseri01@gmail.com>
2019-08-19 21:54:50 +0200

    [Finishes  #167950902] user can view a specific mentor

--- SERVER/api/v1/controllers/mentorCtrl.js
+++ SERVER/api/v1/controllers/mentorCtrl.js
@@ -1,82 +1,86 @@
 import mentors from '../models/mentorModel';
 import users from '../models/userModel';
 import ResponseHandler from '../utils/responseHandler';
-import lodash from 'lodash';
+import renamer from '../utils/renamer';
 
 class Mentor {
 
     static async allMentors(req, res) {
 
         try {
-        
-        users.forEach(user => {
-            if(user.is_mentor === true) {
-                mentors.push(user);
-            }
-        })
 
-        const ObjKeyRename = (src, map) => {
-                const dst = {};
-
-                for (const key in src) {
-                    if (key in map)
-                        // rename key
-                        dst[map[key]] = src[key];
-                    else
-                        // same key
-                        dst[key] = src[key];
+            users.forEach(async user => {
+                if (user.is_mentor === true) {
+                    return mentors.push(user);
                 }
-                return dst;
-        };
+            })
 
-        const uniqueMentors = Array.from(new Set(mentors.map(m => m.id)))
+            const uniqueMentors = Array.from(new Set(mentors.map(m => m.id)))
                 .map(id => {
-                   return  new Promise((resolve, reject)=> {
-                        const currMentor =  mentors.find(m => m.id === id);
-                        const modMentor =  ObjKeyRename(currMentor, { "id": "mentorId" });
+                    return new Promise((resolve, reject) => {
+                        const currMentor = mentors.find(m => m.id === id);
+                        const modMentor = renamer(currMentor, {
+                            "id": "mentorId"
+                        });
                         return resolve(modMentor);
                     })
-                }) 
+                })
 
-        Promise.all(uniqueMentors).then(output => {
-            output.forEach(async obj => {
-               await delete obj['password'];
+            Promise.all(uniqueMentors).then(output => {
+                output.forEach(async obj => {
+                    await delete obj['password'];
+                })
+
+                return res
+                .status(200)
+                .json(new ResponseHandler(200, 'All Mentors', output, null).result());
+                
+            }).catch(err => {
+                console.log(err)
             })
-            return res
-            .status(200)
-            .json(new ResponseHandler(200, 'All Mentors', output, null).result());
-        })
-        
-        } catch (err) {
+
+        }catch (err) {
             return res
                 .status(500)
                 .json(new ResponseHandler(500, err.message, null).result());
 
         }
 
-    }
+    };
 
     static async singleMentor(req, res) {
 
-        const theMentor = mentors.find(m => m.id === parseInt(req.params.id));
+        const modUsers = await users.map(u => {
+            const alteredUser = renamer(u, {"id": "mentorId"});
+            return alteredUser;
+        })
 
+        const theMentor = await  modUsers.find(m => m.mentorId === parseInt(req.params.mentorId));
         try {
 
-            if (!theMentor) return res
+            if(!theMentor || theMentor === undefined) {
+                return res
                 .status(404)
-                .json(new ResponseHandler(404, `Mentor number ${req.params.id} not found`, null).result());
-
-            return res
+                .json(new ResponseHandler(404, `Mentor number ${req.params.mentorId} not found`, null).result());
+    
+            }else if(theMentor.is_mentor !== true){
+                return res
+                .status(404)
+                .json(new ResponseHandler(404, `User number ${req.params.mentorId} is not a mentor`, null).result());
+            }else {
+                delete theMentor['password'];
+                return res
                 .status(200)
                 .json(new ResponseHandler(200, 'Your mentor', theMentor, null).result());
+            }
 
         } catch (err) {
             return res
                 .status(500)
                 .json(new ResponseHandler(500, err.message, null).result())
         }
-
     }
+
 }
 
 
---
+++ SERVER/api/v1/utils/renamer.js
@@ -0,0 +1,13 @@
+const renamer =  (src, map) => {
+        const dst = {};
+        for (const key in src) {
+            if (key in map)
+                // rename key
+                dst[map[key]] = src[key];
+            else
+                // same key
+                dst[key] = src[key];
+        }
+        return dst;
+    }
+export default renamer;
