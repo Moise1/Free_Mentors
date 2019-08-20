ac11b3398065042d581d0cf1e7adb3782eda5391
Moise1 <moiseri01@gmail.com>
2019-08-20 21:51:43 +0200

    [Start #168010966] initial commit

--- SERVER/api/v1/routes/sessionRouter.js
+++ SERVER/api/v1/routes/sessionRouter.js
@@ -20,7 +20,9 @@ sessionRouter.use(json());
 sessionRouter.post("/api/v1/sessions", tokenExists, userAccess, MentorshipSession.createSession);
 sessionRouter.patch("/api/v1/sessions/:sessionId/accept", tokenExists, userAccess, mentorAccess, MentorshipSession.updateSession);
 sessionRouter.patch("/api/v1/sessions/:sessionId/reject", tokenExists, userAccess, mentorAccess, MentorshipSession.updateSession);
-sessionRouter.post("/api/v1/sessions/:sessionId/review", tokenExists, userAccess, menteeAccess, MentorshipSession.reviewMentor);
+sessionRouter.post("/api/v1/sessions/:sessionId/review", tokenExists, userAccess, menteeAccess, MentorshipSession.reviewMentor); 
+sessionRouter.delete("/api/v1/sessions/:sessionId/review", tokenExists, userAccess, adminAccess, MentorshipSession.deleteReview);
+
 
 
 
